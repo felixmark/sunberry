91ec15cb7df98461ffb00ff88e048bbd6b67b756
felixmark <felix.mark@hotmail.com>
2024-06-06 22:23:44 +0200

    Updated the actix server and added stats route.

--- README.md
+++ README.md
@@ -1,12 +1,13 @@
 <h1>Welcome to Sonnenblume!</h1>
 <p>
-    This is a place, where you can program stuff on a solar-powered Raspberry Pi Zero 2 which will hopefully run 24/7.
+    This is a place, where you can program stuff on a solar-powered Raspberry Pi Zero 2 which will hopefully run 24/7.<br>
+    You can just ask for ssh access!
 </p>
 <p>
     <h2>Rules</h2>
     <ul>
         <li>#1: Programs must be written in Rust.</li>
-        <li>#2: Only install packages you need and add them to the list of installed packages.</li>
+        <li>#2: Only install (apt) packages you need and add them to the list of installed packages.</li>
         <li>#3: Keep constant CPU usage under 90%.</li>
         <li>#4: Document as much as possible.</li>
         <li>#5: Don't brick the system (please).</li>
--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,8 @@
-use std::collections::HashMap;
 use actix_files as fs;
-use actix_web::{middleware, web, App, HttpServer, Responder, Result};
-use actix_web_lab::respond::Html;
-use askama::Template;
+use actix_web::{middleware, web, App, HttpServer};
 
-#[derive(Template)]
-#[template(path = "user.html")]
-struct UserTemplate<'a> {
-    name: &'a str,
-    text: &'a str,
-}
-
-#[derive(Template)]
-#[template(path = "index.html")]
-struct Index;
-
-async fn index(query: web::Query<HashMap<String, String>>) -> Result<impl Responder> {
-    let html = if let Some(name) = query.get("name") {
-        UserTemplate {
-            name,
-            text: "Welcome!",
-        }
-        .render()
-        .expect("template should be valid")
-    } else {
-        Index.render().expect("Template should be valid")
-    };
-
-    Ok(Html(html))
-}
+mod index;
+mod stats;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
@@ -39,7 +13,8 @@ async fn main() -> std::io::Result<()> {
     HttpServer::new(move || {
         App::new()
             .wrap(middleware::Logger::default())
-            .service(web::resource("/").route(web::get().to(index)))
+            .service(web::resource("/").route(web::get().to(index::page_index)))
+            .service(web::resource("/stats").route(web::get().to(stats::page_stats)))
             .service(fs::Files::new("/static", "static").show_files_listing())
     })
     .bind(("0.0.0.0", 8080))?
