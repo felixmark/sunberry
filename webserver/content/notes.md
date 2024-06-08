af28f484cf8f68bafe0f5fe9e4c3970bdd23adfa
felixmark <felix.mark@hotmail.com>
2024-06-08 21:19:32 +0200

    Added very cool markdown notes which is getting parsed to html.

---
+++ webserver/content/notes.md
@@ -0,0 +1,5 @@
+# Notes
+
+F1s list
+- Read and display PV    Voltage, Current, Power
+- Read and display RasPi Voltage, Current, Power
--- webserver/src/main.rs
+++ webserver/src/main.rs
@@ -4,6 +4,7 @@ use actix_web::{middleware, web, App, HttpServer};
 mod index;
 mod stats;
 mod links;
+mod notes;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
@@ -17,6 +18,7 @@ async fn main() -> std::io::Result<()> {
             .service(web::resource("/").route(web::get().to(index::page_index)))
             .service(web::resource("/stats").route(web::get().to(stats::page_stats)))
             .service(web::resource("/links").route(web::get().to(links::page_links)))
+            .service(web::resource("/notes").route(web::get().to(notes::page_notes)))
             .service(fs::Files::new("/static", "static").show_files_listing())
             .service(fs::Files::new("/", "static/favicon").show_files_listing())
     })
