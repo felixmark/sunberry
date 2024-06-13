859a866546cde2505374cd28c1f8079412e2bef2
felixmark <felix.mark@hotmail.com>
2024-06-13 22:04:54 +0200

    Cleanup of axum server.

--- projects/webserver/src/main.rs
+++ projects/webserver/src/main.rs
@@ -21,20 +21,18 @@ async fn main() {
     tracing_subscriber::fmt().init();
     tracing::info!("{}", general::separator());
     tracing::info!("Starting Webserver.");
-
     let serve_dir = ServeDir::new("static").not_found_service(ServeFile::new("static"));
-
     let app = Router::new()
         .route("/", get(pages::home::page_home))
         .route("/systeminfo", get(pages::systeminfo::page_systeminfo))
         .route("/book", get(pages::mdpage::page_book))
         .nest_service("/static", serve_dir.clone())
         .fallback(fallback);
-
     let listener = tokio::net::TcpListener::bind("0.0.0.0:80")
         .await
         .unwrap();
-
     tracing::info!("Listening on {}", listener.local_addr().unwrap());
-    axum::serve(listener, app).await.unwrap();
+    axum::serve(listener, app)
+        .await
+        .unwrap();
 }
