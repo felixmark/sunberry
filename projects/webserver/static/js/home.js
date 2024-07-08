cc2323c72f46fdc1b240683e18cdb75681ecb9cb
Felix Mark <felix.mark@hotmail.com>
2024-07-08 12:01:34 +0200

    Correct utc and stuff from sunberry itself.

--- projects/webserver/static/js/home.js
+++ projects/webserver/static/js/home.js
@@ -1,8 +1,71 @@
 // import {de} from './chartjs-adapter-date-fns.bundle.min.js/locale';
 
+const timezoneOffset = new Date().getTimezoneOffset();
+
+Date.prototype.correctUTC = function(timezoneOffset) {
+  this.setTime(this.getTime() - (timezoneOffset*60*1000));
+  return this;
+}
+
+let chartOptions = {
+  interaction: {
+    intersect: false,
+    mode: 'index',
+  },
+  scales: {
+    y: {
+      min: 0,
+      max: 100,
+    },
+    x: {
+      ticks: {
+        includeBounds: true,
+        autoSkip: true,
+        padding: 10,
+        maxRotation: 0,
+        minRotation: 0,
+      },
+      type: 'time',
+      time: {
+        unit: 'minute',
+        displayFormats: {
+          day: 'yy.MM.dd hh:mm'
+        }
+      }
+    }
+  },
+  animation: {
+    duration: 500
+  },
+  maintainAspectRatio: false,
+  cubicInterpolationMode: 'monotone',
+  tension: 0.5,
+  pointRadius: 0,
+  pointHitRadius: 20,
+  plugins: {
+    legend: {
+      labels: {
+        usePointStyle: true,
+        boxHeight: 7,
+      }
+    },
+    tooltip: {
+      usePointStyle: true,
+      multiKeyBackground: "#00000000"
+    }
+  }
+};
+
+// Clone first
+let powerChartOptions = JSON.parse(JSON.stringify(chartOptions));
+
+// Adaptions
+powerChartOptions.scales.y.max = 12;
+
+
 document.addEventListener('DOMContentLoaded', function() {
-  const ctx = document.getElementById('power_chart');
-  let power_chart = new Chart(ctx, {
+  const power_ctx = document.getElementById('power_chart');
+  let power_chart = new Chart(power_ctx, {
     type: 'line',
     data: {
       labels: [],
@@ -22,54 +85,8 @@ document.addEventListener('DOMContentLoaded', function() {
         fill: true,
       }],
     },
-    options: {
-      interaction: {
-        intersect: false,
-        mode: 'index',
-      },
-      scales: {
-        y: {
-          max: 12,
-        },
-        x: {
-          ticks: {
-            maxTicksLimit: 2,
-            callback: function(value) { 
-              return new Date(value).toLocaleDateString('de');
-            },
-          },
-          type: 'time',
-          time: {
-            unit: 'minute',
-            displayFormats: {
-              day: 'yy.MM.dd hh:mm'
-            }
-          }
-        }
-      },
-      animation: {
-        duration: 0
-      },
-      maintainAspectRatio: false,
-      cubicInterpolationMode: 'monotone',
-      tension: 0.5,
-      pointRadius: 0,
-      pointHitRadius: 20,
-      plugins: {
-        legend: {
-          labels: {
-            usePointStyle: true,
-            boxHeight: 7,
-          }
-        },
-        tooltip: {
-          usePointStyle: true,
-          multiKeyBackground: "#00000000"
-        }
-      }
-    }
+    options: powerChartOptions,
   });
-
 
 
 
@@ -106,106 +123,67 @@ document.addEventListener('DOMContentLoaded', function() {
         fill: true,
       }],
     },
-    options: {
-      interaction: {
-        intersect: false,
-        mode: 'index',
-      },
-      scales: {
-        y: {
-          min: 0,
-          max: 100,
-        },
-        x: {
-          ticks: {
-            maxTicksLimit: 2,
-            callback: function(value) { 
-              return new Date(value).toLocaleDateString('de');
-            },
-          },
-          type: 'time',
-          time: {
-            displayFormats: {
-              year: 'YYYY'
-            }
-          }
-        }
-      },
-      animation: {
-        duration: 0
-      },
-      maintainAspectRatio: false,
-      cubicInterpolationMode: 'monotone',
-      tension: 0.5,
-      pointRadius: 0,
-      pointHitRadius: 20,
-      plugins: {
-        legend: {
-          labels: {
-            usePointStyle: true,
-            boxHeight: 7,
-          }
-        },
-        tooltip: {
-          usePointStyle: true,
-          multiKeyBackground: "#00000000"
-        }
+    options: chartOptions
+  });
+
+
+
+
+
+
+
+
+
+
+  function updatePowerChart() {
+    const dbDataRequest = new Request("/api/v1/power_consumption?from=13.06.2024&to=13.06.2024");
+    fetch(dbDataRequest).then((response) => {
+      if (!response.ok) {
+        throw new Error(`HTTP error! Status: ${response.status}`);
       }
-    }
-  });
-
-
-
-
-
-
-
-
-
-
-
-  const dbDataRequest = new Request("/api/v1/power_consumption?from=13.06.2024&to=13.06.2024");
-  fetch(dbDataRequest).then((response) => {
-    if (!response.ok) {
-      throw new Error(`HTTP error! Status: ${response.status}`);
-    }
-    return response.json()
-  }).then((dbDataObject) => {
-    let entries = dbDataObject["data"];
-    power_chart.data.labels = [];
-    power_chart.data.datasets[0].data = [];
-    power_chart.data.datasets[1].data = [];
-    entries.forEach(element => {
-      power_chart.data.labels.push(new Date(element["timestamp"]));
-      power_chart.data.datasets[0].data.push(element["power"]);        // Power used
-      power_chart.data.datasets[1].data.push(element["voltage"]);      // PV power (to be replaced)
+      return response.json()
+    }).then((dbDataObject) => {
+      let entries = dbDataObject["data"];
+      power_chart.data.labels = [];
+      power_chart.data.datasets[0].data = [];
+      power_chart.data.datasets[1].data = [];
+      entries.forEach(element => {
+        power_chart.data.labels.push(new Date(element["timestamp"]).correctUTC(timezoneOffset));
+        power_chart.data.datasets[0].data.push(element["power"]);        // Power used
+        power_chart.data.datasets[1].data.push(element["voltage"]);      // PV power (to be replaced)
+      });
+      power_chart.update();
     });
-    power_chart.update();
-  });
+  }
 
 
 
 
-
-
-  const systemDataRequest = new Request("/api/v1/system?from=13.06.2024&to=13.06.2024");
-  fetch(systemDataRequest).then((response) => {
-    if (!response.ok) {
-      throw new Error(`HTTP error! Status: ${response.status}`);
-    }
-    return response.json()
-  }).then((dbDataObject) => {
-    let entries = dbDataObject["data"];
-    system_chart.data.labels = [];
-    system_chart.data.datasets[0].data = [];
-    system_chart.data.datasets[1].data = [];
-    system_chart.data.datasets[2].data = [];
-    entries.forEach(element => {
-      system_chart.data.labels.push(new Date(element["timestamp"]));
-      system_chart.data.datasets[0].data.push(element["used_memory_percent"]);
-      system_chart.data.datasets[1].data.push(element["cpu_temperature"]);
-      system_chart.data.datasets[2].data.push(element["used_disk_percent"]);
+  function updateSystemChart() {
+    const systemDataRequest = new Request("/api/v1/system?from=13.06.2024&to=13.06.2024");
+    fetch(systemDataRequest).then((response) => {
+      if (!response.ok) {
+        throw new Error(`HTTP error! Status: ${response.status}`);
+      }
+      return response.json()
+    }).then((dbDataObject) => {
+      let entries = dbDataObject["data"];
+      system_chart.data.labels = [];
+      system_chart.data.datasets[0].data = [];
+      system_chart.data.datasets[1].data = [];
+      system_chart.data.datasets[2].data = [];
+      entries.forEach(element => {
+        system_chart.data.labels.push(new Date(element["timestamp"]).correctUTC(timezoneOffset));
+        system_chart.data.datasets[0].data.push(element["used_memory_percent"]);
+        system_chart.data.datasets[1].data.push(element["cpu_temperature"]);
+        system_chart.data.datasets[2].data.push(element["used_disk_percent"]);
+      });
+      system_chart.update();
     });
-    system_chart.update();
-  });
+  }
+
+  updatePowerChart();
+  updateSystemChart();
+  const updatePowerChartInterval = setInterval(updatePowerChart, 5000);
+  const updateSystemChartInterval = setInterval(updateSystemChart, 5000);
 }, false);
