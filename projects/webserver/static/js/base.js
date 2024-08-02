ab89311268758162d7977a327a11cdfbf5b6be0f
felixmark <felix.mark@hotmail.com>
2024-08-02 09:39:20 +0200

    Fixed style for mobile view.

---
+++ projects/webserver/static/js/base.js
@@ -0,0 +1,14 @@
+var menu_visible = false;
+window.onload = function () {
+    const bars_menu = document.getElementById("bars");
+    const menu_wrapper_wrapper = document.getElementById("menu-wrapper-wrapper");
+    bars_menu.onclick = function () {
+        if (menu_visible) {
+            menu_wrapper_wrapper.style.right = "-200px";
+            menu_visible = false;
+        } else {
+            menu_wrapper_wrapper.style.right = "0";
+            menu_visible = true;
+        }
+    }
+};
