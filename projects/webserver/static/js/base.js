768a0d50e829c46ffba381ce6e1a11503eb4ce92
felixmark <felix.mark@hotmail.com>
2024-08-02 15:54:27 +0200

    More minor style changes.

--- projects/webserver/static/js/base.js
+++ projects/webserver/static/js/base.js
@@ -4,7 +4,7 @@ window.onload = function () {
     const menu_wrapper_wrapper = document.getElementById("menu-wrapper-wrapper");
     bars_menu.onclick = function () {
         if (menu_visible) {
-            menu_wrapper_wrapper.style.right = "-200px";
+            menu_wrapper_wrapper.style.right = "-210px";
             menu_visible = false;
         } else {
             menu_wrapper_wrapper.style.right = "0";
